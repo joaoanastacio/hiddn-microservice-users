5810536e31f1e9739b035453d361d76580f33e0c
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-10-19 12:08:50 -0700

    feature: add testing endpoint to complete microservice setup

--- src/main/java/com/hiddn/users/UsersMicroserviceApplication.java
+++ src/main/java/com/hiddn/users/UsersMicroserviceApplication.java
@@ -2,19 +2,15 @@ package com.hiddn.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.hiddn.users")
+@Configuration
 public class UsersMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersMicroserviceApplication.class, args);
-	}
-
-	@GetMapping(path = "/hello")
-	@ResponseBody
-	public String helloWorld() {
-		return "Hello World";
 	}
 }
